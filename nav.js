f8fe973304217582bf9b5ddee669bc9f7fb0162f
njkessel <nathanielkessel@gmail.com>
2026-01-20 15:27:45 -0500

    css stylesheet and navbar js

---
+++ nav.js
@@ -0,0 +1,8 @@
+const navbarHTML = `
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="projects.html">Projects</a></li>
+    </ul>
+`
+
+document.getElementById("nav-placeholder").innerHTML = navbarHTML;
