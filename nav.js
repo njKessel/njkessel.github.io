983131650357cc8fcf8ba59a7cc8fae6f54b88bc
njkessel <nathanielkessel@gmail.com>
2026-01-20 19:32:59 -0500

    Heavy site changes including project page, navigation, featured items, footer.

--- nav.js
+++ nav.js
@@ -1,10 +1,22 @@
 const navbarHTML = `
     <ul>
         <li><a href="index.html" class="home">NATHANIEL KESSEL</a></li>
-        <li><a href="projects.html">PROJECTS</a></li>
+        
+        <li class="dropdown">
+            <a href="projects.html" class="dropbtn">
+                PROJECTS
+            </a>
+
+            <div class="dropdown-content">
+                <a href="#">ESP32 DESK CLOCK</a>
+                <a href="#">PORTFOLIO SITE</a>
+                <a href="#">TI84 BASE CONVERTER</a>
+            </div>
+        </li>
+        
         <li><a href="skills.html">SKILLS</a></li>
         <li class="push-right"><a href="contact.html">CONTACT ME</a></li>
     </ul>
-`
+`;
 
 document.getElementById("nav-placeholder").innerHTML = navbarHTML;
